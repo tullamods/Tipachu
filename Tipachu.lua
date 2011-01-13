71694413433b2aaf3105d68b2a522d991f4c5e34
Tuller <tuller@tullamods.com>
2011-01-12 19:57:31 -0500

    refactored a bit

--- Tipachu.lua
+++ Tipachu.lua
@@ -5,53 +5,47 @@
 
 local DEFAULT_ICON_SIZE = 24
 
-local function addIcon(self, icon)
+--add icon to the tooltip
+local function setTooltipIcon(self, icon)
 	local title = icon and _G[self:GetName() .. 'TextLeft1']
 	if title then
 		title:SetFormattedText('|T%s:%d|t %s', icon, _G['TipachuSize'] or DEFAULT_ICON_SIZE, title:GetText())
 	end
 end
 
---[[ tooltip hooking ]]--
+--tooltip hooking generator
+local function newTooltipHooker(method, func)
+	return function(tooltip)
+		local modified = false
 
-local function hookItem(tip)
-	local tooltipModified = false
+		tooltip:HookScript('OnTooltipCleared', function(self, ...)
+			modified = false
+		end)
 
-	tip:HookScript('OnTooltipCleared', function(self, ...)
-		tooltipModified = false
-	end)
-
-	tip:HookScript('OnTooltipSetItem', function(self, ...)
-		if not tooltipModified  then
-			tooltipModified  = true
-
-			local name, link = self:GetItem()
-			local icon = link and GetItemIcon(link)
-			addIcon(self, icon)
-		end
-	end)
-end
-
-local function hookSpell(tip)
-	local tooltipModified = false
-
-	tip:HookScript('OnTooltipCleared', function(self, ...)
-		tooltipModified = false
-	end)
-
-	tip:HookScript('OnTooltipSetSpell', function(self, ...)
-		if not tooltipModified  then
-			tooltipModified  = true
-
-			local spellName, spellRank, spellID = GameTooltip:GetSpell()
-			if spellId then
-				local icon = select(3, GetSpellInfo(spellID))
-				addIcon(self, icon)
+		tooltip:HookScript(method, function(self, ...)
+			if not modified  then
+				modified = true
+				func(self, ...)
 			end
-		end
-	end)
+		end)
+	end
 end
 
+local hookItem = newTooltipHooker('OnTooltipSetItem', function(self, ...)
+	local name, link = self:GetItem()
+	if link then
+		setTooltipIcon(self, GetItemIcon(link))
+	end
+end)
+
+local hookSpell = newTooltipHooker('OnTooltipSetSpell', function(self, ...)
+	local name, rank, id = self:GetSpell()
+	if id then
+		setTooltipIcon(self, select(3, GetSpellInfo(id)))
+	end
+end)
+
+--hook tooltips
 for _, tooltip in pairs{GameTooltip, ItemRefTooltip} do
 	hookItem(tooltip)
 	hookSpell(tooltip)
